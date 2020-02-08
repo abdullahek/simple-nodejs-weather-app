3cef3d9ec39a4c4e54c00eee5c7112497eb80109
abdullahek <tomsteven86@gmail.com>
2020-02-08 11:01:01 +0500

    initial commit

---
+++ new.php
@@ -0,0 +1,38 @@
+<?php 
+
+$method= $_SERVER['REQUEST_METHOD'];
+
+if($method == "POST")
+{
+	$requestBody = file_get_contents('php://inputs');
+	$json = json_decode($requestBody);
+	$text = $json->queryresult->parameters->text;
+
+	switch ($text) {
+		case 'abd':
+			$speech = "i am abd";
+			break;
+
+		case 'ek':
+			$speech = "i am ek";
+			break;
+
+		
+		default:
+		$speech = "Sorry i didnot get that!";
+			# code...
+			break;
+	}
+
+	$response = new \stdClass();
+
+	$response->speech = "";
+	$response->displayText = "";
+	$response->source = "webhook";
+	echo json_encode($response);
+}
+else {
+	echo "Method not allowed";
+}
+
+?>
